fe8374195d117c3bef38342a7543608b5a00b799
karthik <43676649+brkuhgk@users.noreply.github.com>
2024-04-16 12:14:51 -0400

    Initial commit

---
+++ src/test/java/dosa/DosaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
